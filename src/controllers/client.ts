2fe4ecbdb368e8c90aa1f60f165dd383f5b721d4
sergkryk <sergkryk@ya.ru>
2025-02-05 19:18:27 +0300

    test

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -173,7 +173,10 @@ export const clientGetController = async function (req: Request, res: Response) 
 export const clientPostController = async function (req: Request, res: Response) {
   try {
     validateCredentials(req, res);
+    console.log(req.body);
     const [ account, soap ] = await getClientData({ login: req.body.login, pass: req.body.password });
+    console.log(account);
+    console.log(soap);
     const cookie = soap.getHttpHeaders()?.['set-cookie'];
     setAuthenticatedCookieToClient(res, cookie[0]);
     sendClientData(res, account);
